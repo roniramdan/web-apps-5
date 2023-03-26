49ec3e946eb24779bc6ea18fe859651bd50cf82f
roniramdan <roniramdani1103@gmail.com>
2023-03-26 09:41:11 +0700

    latihan5

---
+++ main.php
@@ -0,0 +1,22 @@
+<?php
+
+$action = $_REQUEST['action'] ? $_GET['action'] : '';
+
+switch($action){
+    case 'add':
+    break;
+
+    case 'update':
+    break;
+
+    case 'delete':
+    break;
+
+    case 'view':
+    break;
+
+    default:
+    break;
+
+}
+?>
